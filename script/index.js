e55f66bdbffaea333220157466a46549dfe949bc
Marc <segardmarc@gmail.com>
2022-11-30 15:28:33 +0100

    change html progress bar by forEach javascript

--- script/index.js
+++ script/index.js
@@ -1,7 +1,63 @@
-const boutonContact = document.getElementById('btn-contact')
-const boutonCV = document.getElementById('btn-cv')
+// const boutonContact = document.getElementById('btn-contact')
+// const boutonCV = document.getElementById('btn-cv')
 
-if (window.screen.width < 1000) {
-    boutonContact.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16"><path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/></svg>'
-    boutonCV.innerText = "CV"
+// if (window.screen.width < 1000) {
+//     boutonContact.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16"><path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/></svg>'
+//     boutonCV.innerText = "CV"
+// }
+
+const experienceGraph2015 = document.getElementsByClassName('row-experience-2015-graph')
+const experienceGraph2018 = document.getElementsByClassName('row-experience-2018-graph')
+const valuePorgressBars = new Map()
+const valuePorgressBars2 = new Map()
+
+valuePorgressBars.set("Windows", "90%")
+valuePorgressBars.set("Linux", "85%")
+valuePorgressBars.set("Python", "80%")
+valuePorgressBars.set("JavaScript", "40%")
+valuePorgressBars.set("Php", "30%")
+valuePorgressBars.set("HTML", "60%")
+valuePorgressBars.set("Suricata", "90%")
+valuePorgressBars.set("Wireshark", "90%")
+
+valuePorgressBars2.set("Linux", "90%")
+valuePorgressBars2.set("C", "85%")
+valuePorgressBars2.set("JIRA", "70%")
+valuePorgressBars2.set("GIT/Gitlab", "55%")
+valuePorgressBars2.set("Docker", "65%")
+valuePorgressBars2.set("Wireshark", "70%")
+valuePorgressBars2.set("Suricata", "80%")
+
+for (const [key,value] of valuePorgressBars) {
+    experienceGraph2015[0].innerHTML +=  
+    `<div class=\"row col-12\">
+    <div class=\"col-4 text-end\">${key}</div>
+    <div class=\"progress col-8 pt-1\">
+      <div
+        class=\"progress-bar\"
+        role=\"progressbar\"
+        style=\"width: ${value}\"
+        aria-valuenow=\"25\"
+        aria-valuemin=\"0\"
+        aria-valuemax=\"100\"
+      ></div>
+    </div>
+    </div>`
+}
+
+for (const [key,value] of valuePorgressBars2) {
+    experienceGraph2018[0].innerHTML += 
+    `<div class=\"row col-12\">
+    <div class=\"col-4 text-end\">${key}</div>
+    <div class=\"progress col-8 pt-1\">
+      <div
+        class=\"progress-bar\"
+        role=\"progressbar\"
+        style=\"width: ${value}\"
+        aria-valuenow=\"25\"
+        aria-valuemin=\"0\"
+        aria-valuemax=\"100\"
+      ></div>
+    </div>
+    </div>`
 }
